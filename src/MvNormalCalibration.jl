9a8d9c5f3d776708812272c843c43e4a519a5e83
Romeo Valentin <romeov@stanford.edu>
2024-05-27 14:22:51 -0700

    Apply JuliaFormatter with SciML style everywhere.

--- src/MvNormalCalibration.jl
+++ src/MvNormalCalibration.jl
@@ -11,63 +11,65 @@ abstract type PredictionSetType end
 
 struct CentralPredictionSet <: PredictionSetType end
 
-computecalibration( preds::AbstractVector{<:Normal},
-                    truevals::AbstractVector{<:Real};
-                    kwargs...) = computecalibration(CentralPredictionSet, preds, truevals; kwargs...)
+function computecalibration(preds::AbstractVector{<:Normal},
+        truevals::AbstractVector{<:Real};
+        kwargs...)
+    computecalibration(CentralPredictionSet, preds, truevals; kwargs...)
+end
 function computecalibration(::Type{CentralPredictionSet},
-                            preds::AbstractVector{<:Normal},
-                            truevals::AbstractVector{<:Real};
-                            pvals=0.0:0.05:1.0)
-  @assert length(preds) .== length(truevals)
-  d = length(first(truevals))
-  @assert all(0 .<= pvals .<= 1)
+        preds::AbstractVector{<:Normal},
+        truevals::AbstractVector{<:Real};
+        pvals = 0.0:0.05:1.0)
+    @assert length(preds) .== length(truevals)
+    d = length(first(truevals))
+    @assert all(0 .<= pvals .<= 1)
 
-  dist_thresholds = quantile.([Normal()], 1/2 .+ pvals./2)
-  containments = map(zip(preds, truevals)) do (pred, trueval)
-      normalized_trueval = (trueval - mean(pred)) / std(pred)
-      abs(normalized_trueval) .<= dist_thresholds
-  end
+    dist_thresholds = quantile.([Normal()], 1 / 2 .+ pvals ./ 2)
+    containments = map(zip(preds, truevals)) do (pred, trueval)
+        normalized_trueval = (trueval - mean(pred)) / std(pred)
+        abs(normalized_trueval) .<= dist_thresholds
+    end
 
-  # Average over samples.
-  # The result is a vector with an average containment rate for each `pval`,
-  # indicating what percentage of samples actually fell in the pvals[0], pvals[1], 
-  # pvals[2], ... percentile prediction set.
-  # If the predictions are well calibrated, then `pvals ≈ calibrationvals` for all indices.
-  # Plotting `plot(pvals, calibrationvals)` should then give a straight line 
-  # from (0, 0), to (1, 1).
-  calibrationvals = mean(containments)
-  @assert length(calibrationvals) == length(pvals)
-  return (; pvals, calibrationvals)
+    # Average over samples.
+    # The result is a vector with an average containment rate for each `pval`,
+    # indicating what percentage of samples actually fell in the pvals[0], pvals[1], 
+    # pvals[2], ... percentile prediction set.
+    # If the predictions are well calibrated, then `pvals ≈ calibrationvals` for all indices.
+    # Plotting `plot(pvals, calibrationvals)` should then give a straight line 
+    # from (0, 0), to (1, 1).
+    calibrationvals = mean(containments)
+    @assert length(calibrationvals) == length(pvals)
+    return (; pvals, calibrationvals)
 end
 
-
-
-computecalibration( preds::AbstractVector{<:AbstractMvNormal},
-                    truevals::AbstractVector{<:AbstractVector{<:Real}};
-                    kwargs...) = computecalibration(CentralPredictionSet, preds, truevals; kwargs...)
+function computecalibration(preds::AbstractVector{<:AbstractMvNormal},
+        truevals::AbstractVector{<:AbstractVector{<:Real}};
+        kwargs...)
+    computecalibration(CentralPredictionSet, preds, truevals; kwargs...)
+end
 function computecalibration(::Type{CentralPredictionSet},
-                            preds::AbstractVector{<:AbstractMvNormal},
-                            truevals::AbstractVector{<:AbstractVector{<:Real}};
-                            pvals=0.0:0.05:1.0)
+        preds::AbstractVector{<:AbstractMvNormal},
+        truevals::AbstractVector{<:AbstractVector{<:Real}};
+        pvals = 0.0:0.05:1.0)
     @assert allequal(length.(truevals))
     @assert length.(preds) == length.(truevals)
     d = length(first(truevals))
     @assert all(0 .<= pvals .<= 1)
-  
-    dist_thresholds = quantile.([Normal()], 1/2 .+ pvals.^(1/d)/2)
+
+    dist_thresholds = quantile.([Normal()], 1 / 2 .+ pvals .^ (1 / d) / 2)
     containments = map(zip(preds, truevals)) do (pred, trueval)
         Λvec, Q = eigen(cov(pred))
-        normalized_trueval = Q*(trueval - mean(pred)) ./ sqrt.(Λvec)
+        normalized_trueval = Q * (trueval - mean(pred)) ./ sqrt.(Λvec)
         # all normalized values have to be in a given distance threshold
-        all(abs.(normalized_trueval) .< dist_thresholds'; dims=1)[:]
+        all(abs.(normalized_trueval) .< dist_thresholds'; dims = 1)[:]
     end
-  
+
     calibrationvals = mean(containments)
     (; pvals, calibrationvals)
 end
 
 function sharpness(pred::Normal)
-    2*std(pred)
+    2 * std(pred)
 end
 
 # Measures volume of 1std ellipsoid.
@@ -75,7 +77,7 @@ function sharpness(pred::AbstractMvNormal)
     Λvec, _Q = eigen(cov(pred))
     d = length(pred)
     # https://en.wikipedia.org/wiki/Ellipsoid#Standard_equation_2
-    π^(d/2)/gamma(d/2+1) * prod(sqrt.(Λvec))
+    π^(d / 2) / gamma(d / 2 + 1) * prod(sqrt.(Λvec))
 end
 
 end # module MvNormalCalibration
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,85 +7,86 @@ using Aqua
 using JET
 
 @testset "MvNormalCalibration.jl" begin
-@testset "Code quality (Aqua.jl)" begin
-    # gotta split this: see https://github.com/JuliaTesting/Aqua.jl/issues/77
-    Aqua.test_all(MvNormalCalibration, ambiguities = false)
-    Aqua.test_ambiguities(MvNormalCalibration)
-end
-
-@testset "Code linting (JET.jl)" begin
-    JET.test_package(MvNormalCalibration;
-        target_defined_modules = true)
-end
-
-@testset "sampling from ground truths" begin
-    @testset "univariate" begin
-        # When the ground truths are sampled exactly from the predictions,
-        # we will always be calibrated, no matter the predictions.
-        (; preds, measurements) = map(1:500_000) do _
-            pred = Normal(rand(), rand()+1.0)
-            measurement = rand(pred)
-            (; preds=pred, measurements=measurement)
-        end |> StructArray
-        (; pvals, calibrationvals) = computecalibration(preds, measurements;
-                                                        pvals=0.00:0.05:1.0)
-        @test pvals ≈ calibrationvals rtol=1e-2
+    @testset "Code quality (Aqua.jl)" begin
+        # gotta split this: see https://github.com/JuliaTesting/Aqua.jl/issues/77
+        Aqua.test_all(MvNormalCalibration, ambiguities = false)
+        Aqua.test_ambiguities(MvNormalCalibration)
     end
 
-    @testset "compare univariate and multivariate " begin
-        (; preds, measurements) = map(1:100) do _
-            pred = Normal(rand(), rand()+1)
-            measurement = rand(pred)
-            (; preds=pred, measurements=measurement)
-        end |> StructArray
-        preds_mv = map(preds) do pred
-            MvNormal([mean(pred)], Diagonal([std(pred)^2]))
-        end
-        measurements_mv = map(measurements) do measurement
-            [measurement]
-        end
-        (; pvals, calibrationvals) = computecalibration(preds, measurements)
-        (; pvals_mv, calibrationvals_mv) = let pvals, calibrationvals
-            (; pvals, calibrationvals) = computecalibration(preds_mv, measurements_mv)
-            (; pvals_mv=pvals, calibrationvals_mv = calibrationvals)
-        end
-        @test pvals == pvals_mv
-        @test calibrationvals ≈ calibrationvals_mv
+    @testset "Code linting (JET.jl)" begin
+        JET.test_package(MvNormalCalibration;
+            target_defined_modules = true)
     end
 
-    @testset "multivariate" begin
-        @testset for d in 1:5
+    @testset "sampling from ground truths" begin
+        @testset "univariate" begin
             # When the ground truths are sampled exactly from the predictions,
             # we will always be calibrated, no matter the predictions.
-            (; preds, measurements) = map(1:100_000) do _
-                Σ = zeros(d,d)
-                while !isposdef(Σ); Σ .= 3*I(d) + hermitianpart!(rand(d,d)); end
-                pred = MvNormal(rand(d), Σ)
+            (; preds, measurements) = map(1:500_000) do _
+                pred = Normal(rand(), rand() + 1.0)
                 measurement = rand(pred)
-                (; preds=pred, measurements=measurement)
+                (; preds = pred, measurements = measurement)
             end |> StructArray
             (; pvals, calibrationvals) = computecalibration(preds, measurements;
-                                                            pvals=0.0:0.02:1.0)
-            @test pvals ≈ calibrationvals rtol=1e-1
+                pvals = 0.00:0.05:1.0)
+            @test pvals≈calibrationvals rtol=1e-2
+        end
+
+        @testset "compare univariate and multivariate " begin
+            (; preds, measurements) = map(1:100) do _
+                pred = Normal(rand(), rand() + 1)
+                measurement = rand(pred)
+                (; preds = pred, measurements = measurement)
+            end |> StructArray
+            preds_mv = map(preds) do pred
+                MvNormal([mean(pred)], Diagonal([std(pred)^2]))
+            end
+            measurements_mv = map(measurements) do measurement
+                [measurement]
+            end
+            (; pvals, calibrationvals) = computecalibration(preds, measurements)
+            (; pvals_mv, calibrationvals_mv) = let pvals, calibrationvals
+                (; pvals, calibrationvals) = computecalibration(preds_mv, measurements_mv)
+                (; pvals_mv = pvals, calibrationvals_mv = calibrationvals)
+            end
+            @test pvals == pvals_mv
+            @test calibrationvals ≈ calibrationvals_mv
+        end
+
+        @testset "multivariate" begin
+            @testset for d in 1:5
+                # When the ground truths are sampled exactly from the predictions,
+                # we will always be calibrated, no matter the predictions.
+                (; preds, measurements) = map(1:100_000) do _
+                    Σ = zeros(d, d)
+                    while !isposdef(Σ)
+                        Σ .= 3 * I(d) + hermitianpart!(rand(d, d))
+                    end
+                    pred = MvNormal(rand(d), Σ)
+                    measurement = rand(pred)
+                    (; preds = pred, measurements = measurement)
+                end |> StructArray
+                (; pvals, calibrationvals) = computecalibration(preds, measurements;
+                    pvals = 0.0:0.02:1.0)
+                @test pvals≈calibrationvals rtol=1e-1
+            end
         end
     end
-end
 
-@testset "sharpness values" begin
-    @testset "univariate" begin
-        for _ in 1:10
-            D = Normal(rand(), rand() + 0.5)
-            @test sharpness(D) ≈ 2*std(D)
+    @testset "sharpness values" begin
+        @testset "univariate" begin
+            for _ in 1:10
+                D = Normal(rand(), rand() + 0.5)
+                @test sharpness(D) ≈ 2 * std(D)
+            end
+        end
+        @testset "multivariate" begin
+            # we know the volume of a circle in 2D / 3D
+            r = 1.5
+            @test sharpness(MvNormal(rand(1), r^2 * I(1))) ≈ 2 * r^1
+            @test sharpness(MvNormal(rand(2), r^2 * I(2))) ≈ π * r^2
+            @test sharpness(MvNormal(rand(2), Diagonal([3^2, 2^2]))) ≈ π * 3 * 2
+            @test sharpness(MvNormal(rand(3), r^2 * I(3))) ≈ (4 / 3 * π * r^3)
         end
     end
-    @testset "multivariate" begin
-        # we know the volume of a circle in 2D / 3D
-        r = 1.5
-        @test sharpness(MvNormal(rand(1), r^2*I(1))) ≈ 2*r^1
-        @test sharpness(MvNormal(rand(2), r^2*I(2))) ≈ π*r^2
-        @test sharpness(MvNormal(rand(2), Diagonal([3^2, 2^2]))) ≈ π*3*2
-        @test sharpness(MvNormal(rand(3), r^2*I(3))) ≈ (4/3*π*r^3)
-    end
-end
-
 end
