a8d7535fee9cab5398ae248552e18088d2ca31a8
Romeo Valentin <romeov@stanford.edu>
2024-05-28 00:20:38 -0700

    Fix typo in deploydocs.

--- docs/make.jl
+++ docs/make.jl
@@ -30,5 +30,5 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/RomeoV/MvNomalCalibration.jl.git";
+    repo = "github.com/RomeoV/MvNormalCalibration.jl.git";
 )
